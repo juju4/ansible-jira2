a907ba5878730bf251da43513529599db2c64547
juju4 <juju4@users.noreply.github.com>
2019-05-11 10:07:05 -0400

    test/default-jira-reverseproxy: fix serverspec tests for centos7

--- test/integration/default-jira-reverseproxy/serverspec/apache_spec.rb
+++ test/integration/default-jira-reverseproxy/serverspec/apache_spec.rb
@@ -10,12 +10,25 @@ set :backend, :exec
 #end
 
 
-describe service('apache2') do
+describe service('httpd'), :if => os[:family] == 'redhat' do
   it { should be_enabled }
   it { should be_running }
-end  
+end
 
-describe command('apache2ctl -M') do
+describe service('apache2'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe command('apachectl -M'), :if => os[:family] == 'redhat' do
+  its(:stdout) { should match /proxy/ }
+  its(:stdout) { should match /remoteip/ }
+  its(:stdout) { should match /rewrite/ }
+  its(:stdout) { should match /ratelimit/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('apache2ctl -M'), :if => os[:family] == 'ubuntu' do
   its(:stdout) { should match /proxy/ }
   its(:stdout) { should match /remoteip/ }
   its(:stdout) { should match /rewrite/ }
