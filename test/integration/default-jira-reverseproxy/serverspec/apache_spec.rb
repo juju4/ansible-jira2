7e43766290a96f76308f9786a97d4a62c71492e7
juju4 <juju4@users.noreply.github.com>
2019-05-11 10:36:41 -0400

    test/default-jira-reverseproxy: fix serverspec tests for centos7 - remove ratelimit module

--- test/integration/default-jira-reverseproxy/serverspec/apache_spec.rb
+++ test/integration/default-jira-reverseproxy/serverspec/apache_spec.rb
@@ -24,7 +24,6 @@ describe command('apachectl -M'), :if => os[:family] == 'redhat' do
   its(:stdout) { should match /proxy/ }
   its(:stdout) { should match /remoteip/ }
   its(:stdout) { should match /rewrite/ }
-  its(:stdout) { should match /ratelimit/ }
   its(:exit_status) { should eq 0 }
 end
 
